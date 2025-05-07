4df3c41994ede60830241734d68a06307cafa364
Misko <misko@meta.com>
2025-05-07 09:01:10 -0700

    license everything (#99)
    * modify all license headers
    
    * update integration test
    
    * move Facebook -> Meta
    
    * revert contributing

--- src/fairchem/core/components/calculate/recipes/energy.py
+++ src/fairchem/core/components/calculate/recipes/energy.py
@@ -1,4 +1,9 @@
 """
+Copyright (c) Meta Platforms, Inc. and affiliates.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+
 Original source: https://github.com/janosh/matbench-discovery/blob/main/matbench_discovery/energy.py
 
 MIT License
--- src/fairchem/core/models/equiformer_v2/layer_norm.py
+++ src/fairchem/core/models/equiformer_v2/layer_norm.py
@@ -1,4 +1,9 @@
 """
+Copyright (c) Meta Platforms, Inc. and affiliates.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+
 1. Normalize features of shape (N, sphere_basis, C),
 with sphere_basis = (lmax + 1) ** 2.
 
