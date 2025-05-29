9089f7db4e5141b687dfc92892edf1d4c94a3281
misko <misko@meta.com>
2025-05-29 01:36:20 +0000

    add pbc v2 grid resolution bounds

--- src/fairchem/core/graph/radius_graph_pbc.py
+++ src/fairchem/core/graph/radius_graph_pbc.py
@@ -7,6 +7,7 @@ LICENSE file in the root directory of this source tree.
 
 from __future__ import annotations
 
+import logging
 import math
 
 import numpy as np
@@ -465,6 +466,10 @@ def radius_graph_pbc_v2(
     # is grid_resolution.
 
     # Compute the grid index for each dimension for each atom
+    max_internal_cell=max(source_atom_pos.max(),target_atom_pos.max())/grid_resolution
+    if max_internal_cell>200:
+        logging.warning("radius graph pbc: This system seems too big, are you using PBC with a large box instead of using PBC=False? Graph generation will be slower than it should be.")
+        grid_resolution=max(source_atom_pos.max(),target_atom_pos.max())/200
     source_atom_grid = torch.floor(source_atom_pos / grid_resolution).long()
     target_atom_grid = torch.floor(target_atom_pos / grid_resolution).long()
 
