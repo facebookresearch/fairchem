cf1eec563c6391a6433bfc1ba93f3440ab300d39
misko <misko@meta.com>
2025-05-30 17:11:14 +0000

    debug

--- src/fairchem/core/models/uma/common/rotation.py
+++ src/fairchem/core/models/uma/common/rotation.py
@@ -149,9 +149,13 @@ def rotation_to_wigner(
         beta_detach[yprod < -YTOL] = math.pi
         beta_detach = beta_detach[~mask]
         for idx in range(alpha.shape[0]):
-            print("1,edge,x,alpha,beta",idx,edge_rot_mat[idx],x[idx],mask[idx],alpha[idx],beta[idx])
+            print("1,edge,x,alpha,beta",idx,edge_rot_mat[idx],x[idx],mask[idx],alpha[idx],beta[idx],flush=True)
             if mask[idx]:
-                torch.autograd.grad(alpha[idx]+beta[idx], x, create_graph=True,allow_unused=True)
+                try:
+                    torch.autograd.grad(alpha[idx]+beta[idx], x, create_graph=True,allow_unused=True)
+                except:
+                    print("FAILED AUTOGRAD!!!",idx,flush=True)
+            print("done",flush=True)
     else:
         for idx in range(alpha.shape[0]):
             print("2,edge,x,alpha,beta",idx,edge_rot_mat[idx],x[idx],alpha[idx],beta[idx])
