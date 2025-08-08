230ef1169a5af1c21c2e2f6a87f5726b46ad61e3
misko <misko@meta.com>
2025-08-08 00:42:07 +0000

    change pbc

--- tests/core/units/mlip_unit/test_embeddings.py
+++ tests/core/units/mlip_unit/test_embeddings.py
@@ -1,12 +1,11 @@
 from __future__ import annotations
 
-import os
 from functools import partial
 
 import torch
+from ase.build import molecule
 
 from fairchem.core import FAIRChemCalculator, pretrained_mlip
-from fairchem.core.datasets.ase_datasets import AseDBDataset
 from fairchem.core.datasets.atomic_data import AtomicData
 from fairchem.core.datasets.collaters.simple_collater import data_list_collater
 from fairchem.core.modules.normalization.normalizer import Normalizer
@@ -14,26 +13,25 @@ from fairchem.core.units.mlip_unit.mlip_unit import Task
 from fairchem.core.units.mlip_unit.predict import AdditionalInferenceTasks
 
 
-def test_get_descriptors(fake_uma_dataset):
-    db = AseDBDataset(config={"src": os.path.join(fake_uma_dataset, "oc20")})
+def test_get_descriptors():
     predictor = pretrained_mlip.get_predict_unit("uma-s-1", device="cpu")
-    atoms = db.get_atoms(0)
+    singlet = molecule("CH4")
+    singlet.center(vacuum=(100))
+    singlet.info.update({"spin": 1, "charge": 0})
+    calc = FAIRChemCalculator(predictor, task_name="omol")
 
-    calc = FAIRChemCalculator(predictor, task_name="oc20")
-
-    embeddings = calc.get_descriptors(atoms)
+    embeddings = calc.get_descriptors(singlet)
     assert "embeddings_layer-1_l0" in embeddings
 
-    embeddings = calc.get_descriptors(atoms, layers_and_ls=[(-1, 1)])
+    embeddings = calc.get_descriptors(singlet, layers_and_ls=[(-1, 1)])
     assert "embeddings_layer-1_l1" in embeddings
 
-    embeddings = calc.get_descriptors(atoms, layers_and_ls=[(-1, 1), (-1, 0)])
+    embeddings = calc.get_descriptors(singlet, layers_and_ls=[(-1, 1), (-1, 0)])
     assert "embeddings_layer-1_l1" in embeddings
     assert "embeddings_layer-1_l0" in embeddings
 
 
-def test_embeddings_from_predict(fake_uma_dataset):
-    db = AseDBDataset(config={"src": os.path.join(fake_uma_dataset, "oc20")})
+def test_embeddings_from_predict():
     layers_and_ls = [(4, 0), (3, 1)]
 
     predictor = pretrained_mlip.get_predict_unit("uma-s-1", device="cpu")
@@ -42,7 +40,7 @@ def test_embeddings_from_predict(fake_uma_dataset):
         predictor.model.module.backbone.get_embedding_head(layers_and_ls=layers_and_ls)
     )
 
-    dataset = "oc20"
+    dataset = "omol"
 
     a2g = partial(
         AtomicData.from_ase,
@@ -53,9 +51,11 @@ def test_embeddings_from_predict(fake_uma_dataset):
         task_name=dataset,
     )
 
-    atoms = db.get_atoms(0)
+    singlet = molecule("CH4")
+    singlet.center(vacuum=(100))
+    singlet.info.update({"spin": 1, "charge": 0})
 
-    batch = data_list_collater([a2g(atoms)], otf_graph=True)
+    batch = data_list_collater([a2g(singlet)], otf_graph=True)
 
     additional_inference_tasks = []
     for layer_idx, l_idx in layers_and_ls:
@@ -80,9 +80,7 @@ def test_embeddings_from_predict(fake_uma_dataset):
     assert "embeddings_layer3_l1" in out
 
 
-def test_embeddings_from_predict_override(fake_uma_dataset):
-    db = AseDBDataset(config={"src": os.path.join(fake_uma_dataset, "oc20")})
-
+def test_embeddings_from_predict_override():
     layers_and_ls = [(4, 0), (3, 1)]
 
     predictor = pretrained_mlip.get_predict_unit(
@@ -98,7 +96,7 @@ def test_embeddings_from_predict_override(fake_uma_dataset):
         },
     )
 
-    dataset = "oc20"
+    dataset = "omol"
 
     a2g = partial(
         AtomicData.from_ase,
@@ -109,9 +107,11 @@ def test_embeddings_from_predict_override(fake_uma_dataset):
         task_name=dataset,
     )
 
-    atoms = db.get_atoms(0)
+    singlet = molecule("CH4")
+    singlet.center(vacuum=(100))
+    singlet.info.update({"spin": 1, "charge": 0})
 
-    batch = data_list_collater([a2g(atoms)], otf_graph=True)
+    batch = data_list_collater([a2g(singlet)], otf_graph=True)
 
     additional_inference_tasks = []
     for layer_idx, l_idx in layers_and_ls:
