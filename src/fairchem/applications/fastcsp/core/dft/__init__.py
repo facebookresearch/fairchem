a66d0b93c4b4b5c7681a93cc0a67df2936281059
Vahe Gharakhanyan <vaheg@meta.com>
2025-08-26 19:24:45 +0000

    update inits

--- src/fairchem/applications/fastcsp/core/dft/__init__.py
+++ src/fairchem/applications/fastcsp/core/dft/__init__.py
@@ -4,4 +4,4 @@ Copyright (c) Meta Platforms, Inc. and affiliates.
 This source code is licensed under the MIT license found in the
 LICENSE file in the root directory of this source tree.
 """
-# Modules related to running and parsing optional DFT simulations with VASP
+# Scripts for running and parsing optional DFT simulations with VASP
