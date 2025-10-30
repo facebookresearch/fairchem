5b3ab8974e0ac6fa4fe12c6311633b9d2e798312
Vahe Gharakhanyan <vaheg@meta.com>
2025-10-30 07:41:46 +0000

    add lic back

--- src/fairchem/applications/fastcsp/core/utils/logging.py
+++ src/fairchem/applications/fastcsp/core/utils/logging.py
@@ -1,4 +1,11 @@
-"""FastCSP Centralized Logging System"""
+"""
+Copyright (c) Meta Platforms, Inc. and affiliates.
+
+This source code is licensed under the MIT license found in the
+LICENSE file in the root directory of this source tree.
+
+FastCSP Centralized Logging System
+"""
 
 from __future__ import annotations
 
