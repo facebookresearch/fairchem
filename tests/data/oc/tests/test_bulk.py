162dca08186d04b86ae83cfcc54b02fe0c436901
bkmi <12955549+bkmi@users.noreply.github.com>
2026-02-18 01:58:53 +0000

    lint

--- tests/data/oc/tests/test_bulk.py
+++ tests/data/oc/tests/test_bulk.py
@@ -99,11 +99,7 @@ class TestBulk:
             precomputed_slabs_dir=precomputed_slabs_dir
         )
 
-        if len(precomputed_slabs) == 15:
-            pytest.xfail(
-                f"Number of generated slabs {len(precomputed_slabs)} is off due to pymatgen bug!"
-            )
-        elif np.__version__.split(".")[0] == "1" and len(precomputed_slabs) == 16:
+        if len(precomputed_slabs) == 15 or np.__version__.split(".")[0] == "1" and len(precomputed_slabs) == 16:
             pytest.xfail(
                 f"Number of generated slabs {len(precomputed_slabs)} is off due to pymatgen bug!"
             )
@@ -127,6 +123,6 @@ class TestBulk:
         max_miller = 0
         for slab in slabs:
             millers = slab.millers
-            max_miller = max(max_miller, max(millers))
+            max_miller = max(max_miller, millers)
 
         return max_miller
