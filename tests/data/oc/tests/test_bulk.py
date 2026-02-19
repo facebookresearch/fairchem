55b578672503cae3071e2b1daa3992d101af4023
bkmi <12955549+bkmi@users.noreply.github.com>
2026-02-19 01:55:10 +0000

    cleanup

--- tests/data/oc/tests/test_bulk.py
+++ tests/data/oc/tests/test_bulk.py
@@ -83,7 +83,7 @@ class TestBulk:
             seen.append(slab)
 
         # pymatgen bug see https://github.com/materialsproject/pymatgen/issues/3747
-        if len(slabs) in [15, 16]:
+        if len(slabs) == 15:
             pytest.xfail(
                 f"Number of generated slabs {len(slabs)} is off due to pymatgen bug!"
             )
