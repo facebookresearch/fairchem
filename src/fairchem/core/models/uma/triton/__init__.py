ee6378a32a84a34dbfbb083c8a5058488a5cf589
misko <misko@meta.com>
2026-02-26 22:14:13 +0000

    Remove HAS_TRITON conditional - assume triton always available

--- src/fairchem/core/models/uma/triton/__init__.py
+++ src/fairchem/core/models/uma/triton/__init__.py
@@ -7,23 +7,14 @@ LICENSE file in the root directory of this source tree.
 
 from __future__ import annotations
 
-try:
-    import triton  # noqa: F401
-
-    HAS_TRITON = True
-except ImportError:
-    HAS_TRITON = False
-
-if HAS_TRITON:
-    from .node_to_edge_wigner_permute import (
-        NodeToEdgeWignerPermuteFunction as UMASFastGPUNodeToEdgeWignerPermute,
-    )
-    from .permute_wigner_inv_edge_to_node import (
-        PermuteWignerInvEdgeToNodeFunction as UMASFastGPUPermuteWignerInvEdgeToNode,
-    )
+from .node_to_edge_wigner_permute import (
+    NodeToEdgeWignerPermuteFunction as UMASFastGPUNodeToEdgeWignerPermute,
+)
+from .permute_wigner_inv_edge_to_node import (
+    PermuteWignerInvEdgeToNodeFunction as UMASFastGPUPermuteWignerInvEdgeToNode,
+)
 
 __all__ = [
-    "HAS_TRITON",
     "UMASFastGPUNodeToEdgeWignerPermute",
     "UMASFastGPUPermuteWignerInvEdgeToNode",
 ]
