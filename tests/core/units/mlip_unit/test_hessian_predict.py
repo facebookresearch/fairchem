40aa329fb6a77c3293e88492d2ae2f7e4005d9a7
lbluque <lbluque@meta.com>
2026-03-06 09:49:16 -0800

    enable hessian tests

--- tests/core/units/mlip_unit/test_hessian_predict.py
+++ tests/core/units/mlip_unit/test_hessian_predict.py
@@ -89,7 +89,6 @@ def get_numerical_hessian(
     return hessian
 
 
-@pytest.mark.xfail(reason="Enable when untrained predictions are implemented")
 @pytest.mark.gpu()
 @pytest.mark.parametrize("vmap", [True, False])
 def test_hessian(vmap):
@@ -125,7 +124,7 @@ def test_hessian(vmap):
     assert np.isfinite(hessian).all()
 
 
-@pytest.mark.xfail(reason="Enable when untrained predictions are implemented")
+@pytest.mark.xfail(reason="Need to fix the numerical/autograd Hessian calculation")
 @pytest.mark.gpu()
 def test_hessian_vs_numerical():
     """Test that analytical and numerical Hessians are close."""
@@ -172,7 +171,6 @@ def test_hessian_vs_numerical():
     )
 
 
-@pytest.mark.xfail(reason="Enable when untrained predictions are implemented")
 @pytest.mark.gpu()
 def test_hessian_symmetry():
     """Test that the Hessian matrix is symmetric."""
