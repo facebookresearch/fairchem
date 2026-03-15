41b56b33fddd732913583830be64e0c9e849ba7a
Ray Gao <rgao@meta.com>
2026-03-15 21:53:04 +0000

    skip a test for now

--- tests/core/units/mlip_unit/test_predict.py
+++ tests/core/units/mlip_unit/test_predict.py
@@ -909,6 +909,7 @@ def test_batch_server_predict_unit_multiple_systems(
 
 
 # this should pass for multi-gpu as well when run locally
+@pytest.mark.skip()
 @pytest.mark.serial()
 @pytest.mark.parametrize("workers", [0, 2])
 @pytest.mark.parametrize("ensemble", ["nvt", "npt"])
