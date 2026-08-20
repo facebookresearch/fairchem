0954053dfa8d6815dab561d03174afb515d5c7fe
lbluque <lbluque@meta.com>
2026-08-20 22:33:15 +0000

    Remove Ray actor concurrency backend

--- src/fairchem/core/calculate/_ray_inference_cluster.py
+++ src/fairchem/core/calculate/_ray_inference_cluster.py
@@ -25,10 +25,6 @@ from typing import Any
 
 import yaml
 
-from fairchem.core.calculate._batch_server import (
-    setup_batch_predict_server,
-    wait_for_serve_ready,
-)
 from fairchem.core.common.utils import recursive_dict_merge
 from fairchem.core.components.batch_server import (
     setup_batch_predict_server,
